c3c6f83eaa47612d74017accf99ea86db6745a97
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2023-05-23 22:58:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <Windows.h>
+#include <psapi.h>
+#include <string>
 
 // Прототип функции обработки сообщений окна
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+// Функция получения информации об использовании памяти
+void GetMemoryInfo(DWORDLONG& totalMemory, DWORDLONG& usedMemory, DWORDLONG& freeMemory);
+
+// Обработчики элементов управления
+void OnPaint(HWND hwnd);
+void OnTimer(HWND hwnd);
+
+// Идентификатор таймера
+constexpr UINT_PTR TIMER_ID = 1;
+
+// Глобальные переменные для хранения информации об использовании памяти
+DWORDLONG g_TotalMemory = 0;
+DWORDLONG g_UsedMemory = 0;
+DWORDLONG g_FreeMemory = 0;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     // Регистрация класса окна
-    const char* className = "HelloWorldWindowClass";
+    const char* className = "MemoryMonitorWindowClass";
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
@@ -13,15 +30,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     RegisterClass(&wc);
 
     // Создание окна
-    HWND hwnd = CreateWindowEx(0, className, "Hello, World!", WS_OVERLAPPEDWINDOW, 
-                               CW_USEDEFAULT, CW_USEDEFAULT, 500, 300, NULL, NULL, hInstance, NULL);
-
-    // Создание кнопки
-    CreateWindow("BUTTON", "Привет", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                 10, 10, 100, 30, hwnd, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowEx(0, className, "Memory Monitor", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 300, 150, NULL, NULL, hInstance, NULL);
 
     // Отображение окна
     ShowWindow(hwnd, nCmdShow);
+
+    // Установка таймера для обновления информации об использовании памяти
+    SetTimer(hwnd, TIMER_ID, 1000, NULL);
 
     // Цикл обработки сообщений окна
     MSG msg = {};
@@ -30,15 +46,55 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         DispatchMessage(&msg);
     }
 
+    // Уничтожение таймера при выходе
+    KillTimer(hwnd, TIMER_ID);
+
     return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
+        case WM_PAINT:
+            OnPaint(hwnd);
+            return 0;
+        case WM_TIMER:
+            OnTimer(hwnd);
+            return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
+}
+
+void GetMemoryInfo(DWORDLONG& totalMemory, DWORDLONG& usedMemory, DWORDLONG& freeMemory) {
+    MEMORYSTATUSEX memoryStatus;
+    memoryStatus.dwLength = sizeof(memoryStatus);
+    GlobalMemoryStatusEx(&memoryStatus);
+
+    totalMemory = memoryStatus.ullTotalPhys;
+    usedMemory = totalMemory - memoryStatus.ullAvailPhys;
+    freeMemory = memoryStatus.ullAvailPhys;
+}
+
+void OnPaint(HWND hwnd) {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+
+    std::string text = "Total Memory: " + std::to_string(g_TotalMemory / (1024 * 1024)) + " MB\n"
+                       "Used Memory: " + std::to_string(g_UsedMemory / (1024 * 1024)) + " MB\n"
+                       "Free Memory: " + std::to_string(g_FreeMemory / (1024 * 1024)) + " MB";
+
+    RECT rect;
+    GetClientRect(hwnd, &rect);
+
+    DrawText(hdc, text.c_str(), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+    EndPaint(hwnd, &ps);
+}
+
+void OnTimer(HWND hwnd) {
+    GetMemoryInfo(g_TotalMemory, g_UsedMemory, g_FreeMemory);
+    InvalidateRect(hwnd, NULL, TRUE);
 }
