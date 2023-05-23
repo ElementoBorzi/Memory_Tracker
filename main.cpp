4930ecc2d23b788771cf1da0086c0abe6ba9a98b
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2023-05-23 22:57:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <Windows.h>
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
+// Прототип функции обработки сообщений окна
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Регистрация класса окна
+    const char* className = "HelloWorldWindowClass";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = className;
+    RegisterClass(&wc);
+
+    // Создание окна
+    HWND hwnd = CreateWindowEx(0, className, "Hello, World!", WS_OVERLAPPEDWINDOW, 
+                               CW_USEDEFAULT, CW_USEDEFAULT, 500, 300, NULL, NULL, hInstance, NULL);
+
+    // Создание кнопки
+    CreateWindow("BUTTON", "Привет", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                 10, 10, 100, 30, hwnd, NULL, hInstance, NULL);
+
+    // Отображение окна
+    ShowWindow(hwnd, nCmdShow);
+
+    // Цикл обработки сообщений окна
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
