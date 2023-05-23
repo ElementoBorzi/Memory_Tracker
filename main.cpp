a2af57aa49e4d0869ca3a675f8451b5e905adc4e
ElementoBorzi <113427648+ElementoBorzi@users.noreply.github.com>
2023-05-23 22:58:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <psapi.h>
 #include <string>
+#include <vector>
 
 // Прототип функции обработки сообщений окна
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -11,14 +12,20 @@ void GetMemoryInfo(DWORDLONG& totalMemory, DWORDLONG& usedMemory, DWORDLONG& fre
 // Обработчики элементов управления
 void OnPaint(HWND hwnd);
 void OnTimer(HWND hwnd);
+void OnCopy(HWND hwnd);
 
 // Идентификатор таймера
 constexpr UINT_PTR TIMER_ID = 1;
+// Идентификатор кнопки "Скопировать"
+constexpr UINT_PTR BUTTON_COPY_ID = 2;
 
 // Глобальные переменные для хранения информации об использовании памяти
 DWORDLONG g_TotalMemory = 0;
 DWORDLONG g_UsedMemory = 0;
 DWORDLONG g_FreeMemory = 0;
+
+// Хранит скопированную информацию о памяти
+std::vector<std::string> g_CopiedMemoryInfo;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     // Регистрация класса окна
@@ -30,8 +37,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     RegisterClass(&wc);
 
     // Создание окна
-    HWND hwnd = CreateWindowEx(0, className, "Memory Monitor", WS_OVERLAPPEDWINDOW | WS_VSCROLL,
-                               CW_USEDEFAULT, CW_USEDEFAULT, 300, 300, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowEx(0, className, "Memory Monitor", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 500, 150, NULL, NULL, hInstance, NULL);
+
+    // Создание кнопки "Скопировать"
+    HWND buttonCopy = CreateWindow("BUTTON", "Copy", WS_TABSTOP | WS_VISIBLE | WS_CHILD,
+                                   10, 10, 80, 30, hwnd, (HMENU)BUTTON_COPY_ID, hInstance, NULL);
 
     // Отображение окна
     ShowWindow(hwnd, nCmdShow);
@@ -60,12 +71,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         case WM_TIMER:
             OnTimer(hwnd);
             return 0;
+        case WM_COMMAND:
+            if (LOWORD(wParam) == BUTTON_COPY_ID) {
+                OnCopy(hwnd);
+                return 0;
+            }
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 void GetMemoryInfo(DWORDLONG& totalMemory, DWORDLONG& usedMemory, DWORDLONG& freeMemory) {
@@ -82,4 +98,45 @@ void OnPaint(HWND hwnd) {
     PAINTSTRUCT ps;
     HDC hdc = BeginPaint(hwnd, &ps);
 
-    std::string text = "Total
+    std::string text = "Total Memory: " + std::to_string(g_TotalMemory / (1024 * 1024)) + " MB\n";
+    text += "Used Memory: " + std::to_string(g_UsedMemory / (1024 * 1024)) + " MB\n";
+    text += "Free Memory: " + std::to_string(g_FreeMemory / (1024 * 1024)) + " MB";
+
+    RECT rect;
+    GetClientRect(hwnd, &rect);
+
+    DrawText(hdc, text.c_str(), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+    EndPaint(hwnd, &ps);
+}
+
+void OnTimer(HWND hwnd) {
+    GetMemoryInfo(g_TotalMemory, g_UsedMemory, g_FreeMemory);
+    InvalidateRect(hwnd, NULL, TRUE);
+}
+
+void OnCopy(HWND hwnd) {
+    std::string memoryInfo = "Total Memory: " + std::to_string(g_TotalMemory / (1024 * 1024)) + " MB\n";
+    memoryInfo += "Used Memory: " + std::to_string(g_UsedMemory / (1024 * 1024)) + " MB\n";
+    memoryInfo += "Free Memory: " + std::to_string(g_FreeMemory / (1024 * 1024)) + " MB";
+
+    // Добавляем информацию в вектор
+    g_CopiedMemoryInfo.push_back(memoryInfo);
+
+    // Копируем информацию в буфер обмена
+    if (OpenClipboard(hwnd)) {
+        EmptyClipboard();
+
+        HGLOBAL hMemory = GlobalAlloc(GMEM_MOVEABLE, memoryInfo.size() + 1);
+        if (hMemory) {
+            char* pMemory = (char*)GlobalLock(hMemory);
+            if (pMemory) {
+                memcpy(pMemory, memoryInfo.c_str(), memoryInfo.size() + 1);
+                GlobalUnlock(hMemory);
+                SetClipboardData(CF_TEXT, hMemory);
+            }
+        }
+
+        CloseClipboard();
+    }
+}
